a56b334d10f14dc282a3a033f377e74adf4fbc77
Kevin Leiva Coto <41813202+kevinleiva97@users.noreply.github.com>
2021-10-26 14:52:07 -0600

    Create Quiz_2_ejer_2.py

---
+++ examen_2/Quiz_2_ejer_2.py
@@ -0,0 +1,8 @@
+from funciones.funciones import *
+
+
+formato_ced(cedu)
+print(f"\n{nombre} {ape} \n{cedu}")
+print("\nEl identificador unico es: ",
+      formato_user(nombre, ape, cedu))
+print("\n")
